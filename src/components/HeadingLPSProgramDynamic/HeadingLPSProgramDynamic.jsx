9205ee19d99ca8c12ce573dca8cec8e45d0f4aa3
Baso-CODE <basoafrizamuhafriza@gmail.com>
2025-11-06 10:08:19 +0700

    update hero dynamic program

--- src/components/HeadingLPSProgramDynamic/HeadingLPSProgramDynamic.jsx
+++ src/components/HeadingLPSProgramDynamic/HeadingLPSProgramDynamic.jsx
@@ -6,11 +6,13 @@ import "slick-carousel/slick/slick.css";
 import { selectContactCsData } from "../../lib/features/contactCsSlice";
 import { useAppSelector } from "../../lib/hooks";
 import "./HeadingLPSProgramDynamic.css";
+import { useEffect, useState } from "react";
 
 const HeadingLPSProgramDynamic = ({
   programName,
   defaultProgramName,
-  imageUrls,
+  imageUrlsDesktop,
+  imageUrlsMobile,
   descriptionParagraphs,
   city,
 }) => {
@@ -25,13 +27,34 @@ const HeadingLPSProgramDynamic = ({
     city = city.charAt(0).toUpperCase() + city.slice(1);
   }
 
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 768);
+
+  useEffect(() => {
+    const handleResize = () => {
+      // Atur breakpoint di sini (misalnya 768px)
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (!city && path.includes("/di/")) {
+    city = path.split("/di/")[1];
+    city = city.charAt(0).toUpperCase() + city.slice(1);
+  }
+
   const finalProgramName =
     programName || defaultProgramName || "Program Les Privat";
 
-  const images =
-    imageUrls && imageUrls.length > 0
-      ? imageUrls
-      : ["/images/program/default.webp"];
+  const images = isMobile
+    ? imageUrlsMobile && imageUrlsMobile.length > 0
+      ? imageUrlsMobile
+      : ["/images/program/default-mobile.webp"]
+    : imageUrlsDesktop && imageUrlsDesktop.length > 0
+    ? imageUrlsDesktop
+    : ["/images/program/default.webp"];
+
   const isMultipleImages = images.length > 1;
 
   // Konfigurasi Slider
@@ -99,12 +122,11 @@ const HeadingLPSProgramDynamic = ({
                 />
               ))
             ) : (
-              // Deskripsi Default/Placeholder
               <>
                 <p className="child-paragraf-hero-program">
                   <span className="color-highlight">
                     Program Intensif {finalProgramName}
-                  </span>{" "}
+                  </span>
                   merupakan suatu bentuk program belajar yang fleksibel dan
                   personal, tersedia secara
                   <span className="color-highlight">
@@ -140,7 +162,8 @@ const HeadingLPSProgramDynamic = ({
 HeadingLPSProgramDynamic.propTypes = {
   programName: PropTypes.string,
   defaultProgramName: PropTypes.string.isRequired,
-  imageUrls: PropTypes.arrayOf(PropTypes.string).isRequired,
+  imageUrlsDesktop: PropTypes.arrayOf(PropTypes.string).isRequired,
+  imageUrlsMobile: PropTypes.arrayOf(PropTypes.string).isRequired,
   descriptionParagraphs: PropTypes.arrayOf(PropTypes.string),
   city: PropTypes.string,
 };

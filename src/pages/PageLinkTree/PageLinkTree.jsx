3e405c66dad6c4702c3df5d5c6689622a334314f
Baso-CODE <basoafrizamuhafriza@gmail.com>
2026-07-15 16:13:00 +0700

    feat: update link icon tiktok

--- src/pages/PageLinkTree/PageLinkTree.jsx
+++ src/pages/PageLinkTree/PageLinkTree.jsx
@@ -61,7 +61,7 @@ const PageLinkTree = () => {
             <Instagram />
           </a>
           <a
-            href="https://www.tiktok.com/@lesprivatsbmptn.com?_r=1&_t=ZS-918OcGnyuIS"
+            href="https://www.tiktok.com/@bimbellps"
             target="_blank"
             rel="noopener noreferrer"
             className="social-icon">
